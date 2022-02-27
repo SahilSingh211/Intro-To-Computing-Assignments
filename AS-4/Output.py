8d8d3a8f2f099c3de658b0a583bc8ad45bdc1721
Sahil <sahilsinghbhandari.bt21ele@pec.edu.in>
2022-02-27 12:01:44 +0530

    Input and Output Files for Assignment 4

---
+++ AS-4/Output.py
@@ -0,0 +1,172 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Assignment 4 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Program 1
+Tower of Hanoi
+
+Enter The number of Disks :6
+Enter the Name of Rod where you want them to be, T for Targeted, A for Assistant :T
+
+Move disk 1 from Start rod to Assistant rod
+Move disk 2 from Start rod to Target rod
+Move disk 1 from Assistant rod to Target rod
+Move disk 3 from Start rod to Assistant rod
+Move disk 1 from Target rod to Start rod
+Move disk 2 from Target rod to Assistant rod
+Move disk 1 from Start rod to Assistant rod
+Move disk 4 from Start rod to Target rod
+Move disk 1 from Assistant rod to Target rod
+Move disk 2 from Assistant rod to Start rod
+Move disk 1 from Target rod to Start rod
+Move disk 3 from Assistant rod to Target rod
+Move disk 1 from Start rod to Assistant rod
+Move disk 2 from Start rod to Target rod
+Move disk 1 from Assistant rod to Target rod
+Move disk 5 from Start rod to Assistant rod
+Move disk 1 from Target rod to Start rod
+Move disk 2 from Target rod to Assistant rod
+Move disk 1 from Start rod to Assistant rod
+Move disk 3 from Target rod to Start rod
+Move disk 1 from Assistant rod to Target rod
+Move disk 2 from Assistant rod to Start rod
+Move disk 1 from Target rod to Start rod
+Move disk 4 from Target rod to Assistant rod
+Move disk 1 from Start rod to Assistant rod
+Move disk 2 from Start rod to Target rod
+Move disk 1 from Assistant rod to Target rod
+Move disk 3 from Start rod to Assistant rod
+Move disk 1 from Target rod to Start rod
+Move disk 2 from Target rod to Assistant rod
+Move disk 1 from Start rod to Assistant rod
+Move disk 6 from Start rod to Target rod
+Move disk 1 from Assistant rod to Target rod
+Move disk 2 from Assistant rod to Start rod
+Move disk 1 from Target rod to Start rod
+Move disk 3 from Assistant rod to Target rod
+Move disk 1 from Start rod to Assistant rod
+Move disk 2 from Start rod to Target rod
+Move disk 1 from Assistant rod to Target rod
+Move disk 4 from Assistant rod to Start rod
+Move disk 1 from Target rod to Start rod
+Move disk 2 from Target rod to Assistant rod
+Move disk 1 from Start rod to Assistant rod
+Move disk 3 from Target rod to Start rod
+Move disk 1 from Assistant rod to Target rod
+Move disk 2 from Assistant rod to Start rod
+Move disk 1 from Target rod to Start rod
+Move disk 5 from Assistant rod to Target rod
+Move disk 1 from Start rod to Assistant rod
+Move disk 2 from Start rod to Target rod
+Move disk 1 from Assistant rod to Target rod
+Move disk 3 from Start rod to Assistant rod
+Move disk 1 from Target rod to Start rod
+Move disk 2 from Target rod to Assistant rod
+Move disk 1 from Start rod to Assistant rod
+Move disk 4 from Start rod to Target rod
+Move disk 1 from Assistant rod to Target rod
+Move disk 2 from Assistant rod to Start rod
+Move disk 1 from Target rod to Start rod
+Move disk 3 from Assistant rod to Target rod
+Move disk 1 from Start rod to Assistant rod
+Move disk 2 from Start rod to Target rod
+Move disk 1 from Assistant rod to Target rod
+
+All of the disks have been moved from Start rod to Target rod
+
+_._._._._._._._._._._._._._._._._._._._._._._._._._._._._._.
+
+Program 2
+Pascal's Triangle
+
+Enter the number of rows you want :8
+
+The Pascal's Triangle by Recursive method is as follows :
+
+                                1
+                            1      1
+                        1      2      1
+                    1      3      3      1
+                1      4      6      4      1
+            1      5      10     10     5      1
+        1      6      15     20     15     6      1
+    1      7      21     35     35     21     7      1
+
+The Pascal's Triangle made in Iterative Format is :
+
+                                      1
+                                  1      1
+                              1      2      1
+                          1      3      3      1
+                      1      4      6      4      1
+                  1      5     10     10      5      1
+              1      6     15     20     15      6      1
+          1      7     21     35     35     21      7      1
+
+_._._._._._._._._._._._._._._._._._._._._._._._._._._._._._.
+
+Program 3
+Built-in Functions
+
+Enter first number :63
+Enter Second number :3
+
+The quotient and Remainder are as follows : (21, 0)
+Part(a)
+The statement that the function is Callable is True
+The statement that the function with values is Callable is False
+
+Part(b)
+There are zeroes in List
+
+Part(c)
+Numbers greater than 4 are : {21, 5, 6}
+
+Part(d)
+The immutable set is : frozenset({21, 5, 6})
+
+Part(e)
+Maximum Value in the set is : 21
+
+Part(f)
+Hash of max value is : 21
+
+_._._._._._._._._._._._._._._._._._._._._._._._._._._._._._.
+
+Program 4
+Student's Records creator
+
+
+Enter Name of Student :Sahil Singh
+Enter Sahil singh's SID :21104050
+Created Record for Sahil singh
+
+Student's Name is Sahil singh, and Roll number is 21104050.
+
+_._._._._._._._._._._._._._._._._._._._._._._._._._._._._._.
+
+Program 5
+Employee name salary Records
+
+| Employee   <==>   Salary   |
+|   Mehak    <==>    40000   |
+|   Ashok    <==>    50000   |
+|   Viren    <==>    60000   |
+
+The salary of Mehak is updated to 70000
+
+Viren's Record is deleted
+
+_._._._._._._._._._._._._._._._._._._._._._._._._._._._._._.
+
+Program 6
+Friendship Checker
+
+Enter George's Word :Silent
+Enter Barbie's Word :Listen
+
+George and Barbie have False friendship.
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ End Of Assignment ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Destroyed the record of Sahil singh
---
+++ AS-4/input.py
@@ -0,0 +1,270 @@
+print("~"*30,"Assignment 4","~"*30)
+print()
+
+print("Program 1\nTower of Hanoi")
+print()
+
+# Program to solve the problem for Tower of Hanoi.
+
+# Defining a function for movement of rods in Tower of Hanoi.
+def TowerOfHanoi(n: int, Starting: str, Target: str, Assistant: str):
+    # Starting with first disk.
+    if n == 1:
+        print("Move disk 1 from {0} rod to {1} rod".format(Starting, Target))
+
+        return
+    # Recursive approach for nth disk.
+
+    TowerOfHanoi(n - 1, Starting, Assistant, Target)
+    print("Move disk {0} from {1} rod to {2} rod".format(n, Starting, Target))
+    TowerOfHanoi(n - 1, Assistant, Target, Starting)
+
+
+Disk_num = int(input("Enter The number of Disks :"))
+
+Entry = "Start"  # Starting Rod
+Exit = input("Enter the Name of Rod where you want them to be, T for Targeted, A for Assistant :")
+Exit = Exit.upper().strip()
+print()
+
+if Exit == "T":
+    Exit = "Target"
+    Helper = "Assistant"
+
+elif Exit == "A":
+    Exit = "Assistant"
+    Helper = "Target"
+
+else:
+    print("Wrong Rod Chosen")
+    exit()
+
+TowerOfHanoi(Disk_num, Entry, Exit, Helper)
+print()
+
+print("All of the disks have been moved from {} rod to {} rod".format(Entry, Exit))
+
+print()
+print("_."*30)
+print()
+
+print("Program 2\nPascal's Triangle")
+# Program To create Pascal's Triangle by both Recursive and Iterative way.
+
+print()
+
+
+# Defining a function for factorial of number using recursion.
+
+def Fac(Number):
+    if Number == 0:
+        return 1
+    else:
+        b = (Number * Fac(Number - 1))
+    return b
+
+
+# Asking user to input Number of rows for Pascal Triangle.
+num = int(input("Enter the number of rows you want :"))
+print()
+list1 = []
+# Building numbers for Pascal Triangle.
+for n in range(0, num):
+    L = []
+    for i in range(n + 1):
+        L.append(str(int(Fac(n)) // (int(Fac(n - i)) * (int(Fac(i))))))
+    list1.append(L)
+
+# making the Triangle.
+
+print("The Pascal's Triangle by Recursive method is as follows :")
+print()
+
+for i in range(num):
+    print("    " * (num - i), end="")
+    for j in range(0, i + 1):
+        print('{0:6}'.format(list1[i][j]), end=" ")
+    print()
+print()
+
+# Making Pascal's Triangle the Iterative way.
+
+Tri_List = []
+# An empty list
+
+for i in range(num):
+    Tri_List.append([])
+    Tri_List[i].append(1)
+
+    # Above lines make nested list of same number as input.
+
+    for j in range(1, i):
+        Tri_List[i].append(Tri_List[i - 1][j - 1] + Tri_List[i - 1][j])
+
+    if num != 0:
+        Tri_List[i].append(1)
+
+print("The Pascal's Triangle made in Iterative Format is :")
+print()
+
+for i in range(num):
+    print("    " * (num - i), end=" ")
+
+    for j in range(0, i + 1):
+        print('{0:6}'.format(Tri_List[i][j]), end=" ")
+
+    print()
+
+print()
+print("_."*30)
+print()
+
+print("Program 3\nBuilt-in Functions")
+# Programs to find quotient, remainder and use in-built Function.
+print()
+
+# Asking User to input Numbers.
+num1 = int(input("Enter first number :"))
+num2 = int(input("Enter Second number :"))
+print()
+
+# Using Div-mod function for Quotient and remainder.
+Quotient_Remainder = (divmod(num1, num2))
+
+# Lists to use in future parts of Question.
+List2 = []
+print("The quotient and Remainder are as follows :", Quotient_Remainder)
+
+List1 = list(Quotient_Remainder)
+List3 = list(Quotient_Remainder)
+# part a.
+print("Part(a)\nThe statement that the function is Callable is", callable(divmod))
+print("The statement that the function with values is Callable is", callable(divmod(num1, num2)))
+print()
+
+# Part B, Counting zeroes in L.
+print("Part(b)")
+if 0 in List1:
+    print("There are zeroes in List")
+else:
+    print("There are no zeroes in List")
+print()
+
+# part c, Printing numbers greater than 4.
+for j in [4, 5, 6]:
+    List1.append(j)
+for k in range(len(List1)):
+    if List1[k] > 4:
+        List2.append(List1[k])
+
+List2.sort()
+Set_of_num = set(List2)
+print("Part(c)\nNumbers greater than 4 are :", Set_of_num)
+print()
+
+
+
+Froze_Set = frozenset(Set_of_num)
+
+# part d.
+print("Part(d)\nThe immutable set is :", Froze_Set)
+print()
+
+# part e.
+print("Part(e)\nMaximum Value in the set is :", max(Froze_Set))
+print()
+
+# Part f.
+print("Part(f)\nHash of max value is :", hash(max(Froze_Set)))
+
+print()
+print("_."*30)
+print()
+
+print("Program 4\nStudent's Records creator")
+# Program for creating and deleting a student's record.
+print()
+
+
+# Creating a Class for student's Records.
+class Student:
+    def __init__(self, Name: str, Roll: int):
+        self.name = Name
+        self.roll = Roll
+        print("Created Record for", self.name)
+
+    def __del__(self):
+        print("Destroyed the record of", self.name)
+
+
+print()
+
+# Taking input from user.
+Name_ = input("Enter Name of Student :").capitalize()
+SID = int(input("Enter {}'s SID :".format(Name_)))
+
+S1 = Student(Name_, SID)
+print()
+
+print("Student's Name is {0}, and Roll number is {1}.".format(Name_, SID))
+
+print()
+print("_."*30)
+print()
+
+print("Program 5\nEmployee name salary Records")
+print()
+
+# Creating a class for Employees.
+
+class Employee:
+    def __init__(self, name: str, salary: int):
+        self.name = name  # Name of Employee
+        self.salary = salary  # Salary of Employee
+
+
+# Creating Records for Employees.
+E1 = Employee("Mehak", 40000)
+E2 = Employee("Ashok", 50000)
+E3 = Employee("Viren", 60000)
+
+# Making a Table for Employees and their respective salaries.
+print(
+    "| Employee   <==>   Salary   |\n|   {}    <==>    {}   |\n|   {}    <==>    {}   |\n|   {}    <==>    {}   |".format(
+        E1.name, E1.salary,
+        E2.name, E2.salary,
+        E3.name, E3.salary))
+
+print()
+# part(a)
+E1.salary = 70000
+print("The salary of {} is updated to {}".format(E1.name, E1.salary))
+print()
+
+# part(b)
+print("{0}'s Record is deleted".format(E3.name))
+del E3
+
+print()
+print("_."*30)
+print()
+
+print("Program 6\nFriendship Checker")
+print()
+
+# Taking input for both George's and Barbie's word
+George = input("Enter George's Word :".strip())
+Barbie = input("Enter Barbie's Word :".strip())
+
+# Making a list of all alphabets in both words and sorting them.
+G = list(George)
+B = list(Barbie)
+
+G.sort()
+B.sort()
+
+print()
+print(f"George and Barbie have {G==B} friendship.")
+print()
+
+print("~"*30,"End Of Assignment","~"*30)
